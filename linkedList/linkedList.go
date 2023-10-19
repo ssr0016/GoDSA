3c647c27e3939f2f9afddbeea6512c7f60d5f84a
ssr0016 <samsonrecluta0016@gmail.com>
2023-10-19 15:44:00 +0800

    first commit

---
+++ linkedList/linkedList.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type node struct {
+	data int
+	next *node
+}
+
+type linkedList struct {
+	head   *node
+	length int
+}
+
+func (l *linkedList) prepend(n *node) {
+	second := l.head
+	l.head = n
+	l.head.next = second
+	l.length++
+}
+
+func main() {
+	myList := linkedList{}
+	node1 := &node{data: 48}
+	node2 := &node{data: 18}
+	node3 := &node{data: 16}
+	myList.prepend(node1)
+	myList.prepend(node2)
+	myList.prepend(node3)
+	fmt.Println(myList)
+
+}
